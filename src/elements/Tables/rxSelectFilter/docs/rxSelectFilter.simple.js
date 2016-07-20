6c80f94aee074c5360db4aeb32ea08afbd664e5c
satyam kotakonda <satyam.kotakonda@rackspace.com>
2016-07-20 19:20:17 +0530

    refactor(framework): filter the table

--- src/elements/Tables/rxSelectFilter/docs/rxSelectFilter.simple.js
+++ src/elements/Tables/rxSelectFilter/docs/rxSelectFilter.simple.js
@@ -1,5 +1,5 @@
 angular.module('demoApp')
-.controller('rxSelectFilterCtrl', function ($scope, SelectFilter) {
+.controller('rxSelectFilterSimpleExampleCtrl', function ($scope, SelectFilter) {
     $scope.filter = SelectFilter.create({
         properties: ['account', 'status'],
         selected: {
